567f4893c27a0982f1b2ee0c223e2de068646573
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-11-02 00:11:43 +0530

    footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Box } from "@mui/material";
+
+import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box className={classes.footer}>
+      <Box className={classes.top}>
+        <Box className={classes.item}>
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Kids</span>
+          <span>Footwear</span>
+          <span>Accessories</span>
+          <span>NEW ARRIVALS</span>
+        </Box>
+
+        <Box className={classes.item}>
+          <h1>Links</h1>
+          <span>FAQS</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </Box>
+
+        <Box className={classes.item}>
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Necessitatibus distinctio facere iste quod sequi illo corporis,
+            labore nemo eligendi modi quibusdam eius accusamus velit commodi
+            dolores debitis culpa voluptatibus esse.
+          </span>
+        </Box>
+        <Box className={classes.item}>
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Necessitatibus distinctio facere iste quod sequi illo corporis,
+            labore nemo eligendi modi quibusdam eius accusamus velit commodi
+            dolores debitis culpa voluptatibus esse.
+          </span>
+        </Box>
+      </Box>
+      <Box className={classes.bottom}>
+        <Box className={classes.left}>
+          <span className={classes.logo}>StyleStore</span>
+          <span className={classes.copyright}>
+            {" "}
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
